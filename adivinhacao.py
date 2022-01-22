42db1b1034dbaebadee47ecf60dec56b5f29bd75
mauriciosm13 <70268675+mauriciosm13@users.noreply.github.com>
2022-01-22 12:15:02 -0300

    Commit
    Definindo pontos e niveis no jogo

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,7 +5,21 @@ print("Bem vindo no jogo de adivinhação")
 print("********************************")
 
 numero_secreto = random.randrange(1, 101)
-total_tentativas = 3
+total_tentativas = 0
+pontos = 1000
+
+print("Qual nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel_input = int(input("Defina um nível: "))
+if(nivel_input == 1):
+    total_tentativas = 20
+elif(nivel_input == 2):
+    total_tentativas = 10
+elif(nivel_input == 3):
+    total_tentativas = 5
+else:
+    print("Necessário selecionar um nível valido")
 
 for rodada in range(1, total_tentativas + 1):
     print("Tentativa: {} de  {}".format(rodada, total_tentativas))
@@ -19,12 +33,15 @@ for rodada in range(1, total_tentativas + 1):
     maior = chute > numero_secreto
 
     if (acertou):
-        print("Parabéns! Você acertou")
+        print("Parabéns! Você acertou -- Pontuação total foi de {}".format(pontos))
         break
     else:
         if(maior):
             print("Você errou! O seu chute foi maior que número secreto")
         else:
             print("Você errou! O seu chute foi menor que o número secreto")
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
 
+print("Numéro secreto é: ",numero_secreto)
 print("Fim de jogo")
